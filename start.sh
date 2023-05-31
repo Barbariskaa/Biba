697d34715f74b0c2896d18a4db79e749e21720cf
Barbariskaa <129290831+Barbariskaa@users.noreply.github.com>
2023-05-31 07:27:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python -m venv venv
-source venv/bin/activate
+source venv/bin/activate # Команда на случай если ругается на venv: sudo apt install python3.10-venv
 pip install -r requirements.txt
 python main.py
